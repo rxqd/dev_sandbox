cb76835c17db27fe46bf09c152c94549d2c38e4b
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-11 11:38:32 +0200

    Add replit config

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$1" == "javascript" ]]; then
+  cd javascript
+  node "$2".js $3 $4 # Pass additional arguments if needed
+elif [[ "$1" == "rust" ]]; then
+  cd rust
+  cargo run $2 $3
+elif [[ "$1" == "python" ]]; then
+  cd python
+  python3 "$2".py $3 $4
+else
+  echo "Invalid language specified."
+fi
