cfb07094ff96198429a2b34ee1a883a0b5bb16b3
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-12 09:38:07 +0200

    Remove symlink if exists

--- current.sh
+++ current.sh
@@ -5,14 +5,27 @@ if [[ ! -e "$1" ]]; then
   exit 1
 fi
 
-ln -sf "$1" current  
+if [[ -L current ]]; then 
+  rm current
+fi
+
+ln -s "$1" current  
 
 if [[ ! -e "current/replit.toml" ]]; then
   echo "$1/replit.toml must exist"
   exit 1
 fi
 
-ln -sf "current/replit.toml" ".replit"
+if [[ -L ".replit" ]]; then 
+  rm ".replit"
+fi
+
+ln -s "current/replit.toml" ".replit"
+
+
+if [[ -L "replit.nix" ]]; then 
+  rm "replit.nix"
+fi
 
 if [[ -e "current/replit.nix" ]]; then
   ln -sf "current/replit.nix" "replit.nix"
