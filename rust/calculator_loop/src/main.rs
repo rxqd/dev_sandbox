3fd92d19e80544c6f60539d46178f3df8e4d05a6
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-13 23:18:34 +0200

    Create structure for calculator loop

---
+++ rust/calculator_loop/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+fn main() {
+    let args_line: String = read_args();
+
+    do_main(&args_line);
+}
+
+# TODO: read args line like 1+3-7+8
+# validate line with rules
+# use create persentage func %
+# ability to calculate proportion like prop 2 = x | 3 = 5
+fn read_args() -> String {
+  # infinite loop
+  # read line
+  # calculate result
+  # continue
+  # remember previous results
+}
