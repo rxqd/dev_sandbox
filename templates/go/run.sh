65e4e5c2759b8608828d0d12079b876d6d5ab337
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-16 23:18:34 +0200

    Remove go

--- templates/go/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-current_dir=$(dirname "$(readlink -f "$0")")
-go run "$current_dir/src/main.go"
--- templates/nodejs/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-current_dir=$(dirname "$(readlink -f "$0")")
-node "$current_dir/src/index.js"
