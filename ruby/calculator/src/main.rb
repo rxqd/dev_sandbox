95a111e963749b0f7a4101008e32917454df2df6
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-16 16:38:12 +0200

    Extract loop logic to App class

--- ruby/calculator/src/main.rb
+++ ruby/calculator/src/main.rb
@@ -1,34 +1,10 @@
+require_relative "app"
 require_relative "calculator"
 require "readline"
 
-stty_save = `stty -g`.chomp
-trap("INT") do
-  system "stty", stty_save
-  exit
-end
-
-def remove_history_duplicates(buf)
-  Readline::HISTORY.pop if /^\s*$/ =~ buf
-  if Readline::HISTORY[Readline::HISTORY.length - 2] == buf
-    Readline::HISTORY.pop
-  end
-# rubocop:disable Lint/SuppressedException
-rescue IndexError
-end
-# rubocop:enable Lint/SuppressedException
-
-PROMPT = "$= ".freeze
-
 def main
-  while (buf = Readline.readline(PROMPT, true))
-    remove_history_duplicates(buf)
-
-    if %w[q quit exit].include?(buf)
-      p "Bye!"
-      break
-    end
-
-    p Calculator.new.calculate(buf)
+  App.run do |user_input|
+    Calculator.new.calculate(user_input)
   end
 end
 
