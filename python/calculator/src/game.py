a0a4f2e175a4c8639fa4f4c3e63840d466dc5fb3
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-18 03:43:14 +0200

    Add play logic

--- python/calculator/src/game.py
+++ python/calculator/src/game.py
@@ -18,11 +18,24 @@ def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
+def choose(items):
+    """Choose and return a random item"""
+    return random.choice(items)
+
+def player_order(names, start=None):
+    """Rotate player order so that start goes first"""
+    if start is None:
+        start = choose(names)
+    start_idx = names.index(start)
+    return names[start_idx:] + names[:start_idx]
+
 def play() -> None:
     """Play a 4-player card game"""
     deck: Deck = create_deck(shuffle=True)
     names: List = "P1 P2 P3 P4".split()
     hands: Dict = {n: h for n, h in zip(names, deal_hands(deck))}
+    start_player = choose(names)
+    turn_order = player_order(names, start=start_player)
 
     for name, cards in hands.items():
         card_str = " ".join(f"{s}{r}" for (s, r) in cards)
