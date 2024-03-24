1228f94aa95217c4a3b4f2c1d68a5bc1b4cef738
Maksym Litvynov <rxx@users.noreply.github.com>
2024-03-24 12:39:25 +0200

    Update rust template

--- templates/rust/src/main.rs
+++ templates/rust/src/main.rs
@@ -1,4 +1,18 @@
 
 fn main() {
-  println!("Hello world from rust!");
+    println!(hello());
+}
+
+pub fn hello() -> 'static &str {
+  "Hello rust"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello rust");
+    }
 }
