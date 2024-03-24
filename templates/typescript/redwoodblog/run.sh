61a94ee6cdea7291af868e94bd396f91715b39d5
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-24 21:22:30 +0200

    Fix redwood run.sh

--- templates/typescript/redwoodblog/run.sh
+++ templates/typescript/redwoodblog/run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# abort on nonzero exitstatus
+# abort on unbound variable
+# don't hide errors within pipes
+set -euo pipefail
+
 current_dir=$(dirname "$(readlink -f "$0")")
-PWD=$current_dir yarn rw dev
+
+yarn --cwd="$current_dir" rw dev
