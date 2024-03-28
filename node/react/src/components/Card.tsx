f14ba86b542580d7aee3768e3f913852aba82160
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-28 08:12:08 +0000

    Update python module

--- node/react/src/components/Card.tsx
+++ node/react/src/components/Card.tsx
@@ -2,10 +2,9 @@ const Card = () => {
   return (
       <div className="card">
         <h2>Card</h2>
-        <p>This is card</p>
+        <p>Description</p>
     </div>
   )
 }
 
 export default Card;
-
